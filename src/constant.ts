a0ecdb526bef5d0a693c81a62eab588a560da0f7
root <root@pi-syna>
2023-04-19 03:18:08 -0700

    update title

--- src/constant.ts
+++ src/constant.ts
@@ -4,8 +4,8 @@ export const Attributes = {
   "plugin": "@webds/tutor_initial_setup:plugin",
   "command": "webds_tutor_initial_setup:open",
   "id": "webds_tutor_initial_setup_widget",
-  "label": "Initial Setup",
-  "caption": "Initial Setup",
+  "label": "Local CBC and CSat",
+  "caption": "Local CBC and CSat",
   "category": "Device - Config Library",
   "rank": 30,
   "icon": extensionTutorInitialSetupIcon
